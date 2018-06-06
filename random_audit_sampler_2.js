3332190ffff9f64c929e7799953d46248aa0c14c
kathryn <kathrynchew@gmail.com>
2018-06-06 00:48:50 +0000

    +re-run one sample; +output full category set; +detect audit start & prevent overwrite of sample

--- random_audit_sampler_2.js
+++ random_audit_sampler_2.js
@@ -4,16 +4,31 @@
 
 /* SETUP GOES HERE */
 
-function onOpen() {    // Create new menu item to launch sampler
-  SpreadsheetApp.getUi()
-    .createMenu('Audit')
-    .addItem('Generate All Samples', 'onboardingTransactionSample') // Run all sampler functions
-    .addToUi();
+function onOpen() {
+  var ui = SpreadsheetApp.getUi()
+  ui.createMenu('Audit')
+  .addItem('Generate All Samples', 'onboardingTransactionSample') // Run all sampler functions
+  .addSeparator()
+  .addSubMenu(ui.createMenu('Output Category Data')
+              .addItem('[ALL] Funded - No PSL - CRB', 'writeNoPslCrbAll')
+              .addItem('[ALL] Funded - No PSL - BankMobile','writeNoPslBmAll')
+              .addItem('[ALL] Funded - PSL - CRB','writeYesPslCrbAll')
+              .addItem('[ALL] Declined - CRB','writeDeclinedCrbAll')
+              .addItem('[ALL] Declined - BankMobile','writeDeclinedBmAll')
+             )
+  .addSubMenu(ui.createMenu('Re-Run One Sample Set')
+              .addItem('Funded - No PSL - CRB', 'runNoPslCrb')
+              .addItem('Funded - No PSL - BankMobile', 'runNoPslBm')
+              .addItem('Funded - PSL - CRB', 'runYesPslCrb')
+              .addItem('Declined - CRB', 'runDeclinedCrb')
+              .addItem('Declined - BankMobile', 'runDeclinedBm')
+             )
+  .addToUi();
 }
 
-function getAllValues() {    // Fetch all values from intial raw data csv content
+function getAllValues() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Raw Data");
-  var startRow = 2;
+  var startRow = 1;
   var lastRow = sheet.getLastRow();
   lastRow = lastRow - startRow + 1;
   var startCol = 1;
@@ -65,7 +80,7 @@ function getDeclinedCrb(allValues) { // Filter for all loans that meet parameter
   return filteredValues;  
 }
 
-function getDeclinedBm(allValues) {    // Filter for all loans that meet parameters: student_loan == No, saas_institution_id == 1, loan_status == funded
+function getDeclinedBm(allValues) { // Filter for all loans that meet parameters: student_loan == No, saas_institution_id == 1, loan_status == funded
   var filteredValues = [];
   for (i=0; i<allValues.length; i++) {
     if (allValues[i][1]==3 && allValues[i][5]==='No' && (allValues[i][2]==='declined' || allValues[i][2]==='disqualified')) {
@@ -105,39 +120,141 @@ function randomBmSample(bmData) {    // Randomly select 5 rows from filtered set
 /* LOGIC TO WRITE FILTERED VALUES TO SHEETS GOES HERE */
 
 function writeSampleToSheet(sampleData, sheetName) {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = ss.getSheetByName(sheetName);
-  if (sampleData.length > 0) {
-    var startRow = 6;
-    var startCol = 1;
-    var lastRow = sampleData.length;
-    var lastCol = sampleData[0].length;
-    var range = sheet.getRange(startRow, startCol, lastRow, lastCol).setValues(sampleData); 
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName(sheetName);
+  var currentContents = sheet.getRange("Z6:AO15").getValues();
+  var contentCount = 0;
+                                                                             // Arrays to confirm re-running audit sample will not                 
+  var fivePlaceholders = ["Funded - NO PSL - BankMobile"];                   // overwrite an audit that has already started
+  var tenPlaceholders = ["Funded - NO PSL - CRB", "Funded - PSL - CRB", "Declined - BankMobile"];
+  var twentyPlaceholders = ["Declined - CRB"];
+  
+  for (var i=0;i<currentContents.length;i++) {                               // Count how many fields in the sheet have been filled in   
+    for (var a=0;a<currentContents[0].length;a++) {
+      if (currentContents[i][a].length > 0) {
+        contentCount++;
+      }
+    }
+  }
+  
+  if (contentCount > 10 && tenPlaceholders.indexOf(sheetName) != -1) {            // If a sheet that should only have 10 (auto)filled 
+    var ui = SpreadsheetApp.getUi();                                              // audit cells has more, throw an alert
+    ui.alert("Auditing of the current dataset [[ " + sheetName + " ]] has already begun. Sample cannot be re-generated.");
+  }
+  else if (contentCount > 5 && fivePlaceholders.indexOf(sheetName) != -1) {       // If a sheet that should only have 5 (auto)filled 
+    var ui = SpreadsheetApp.getUi();                                              // audit cells has more, throw an alert
+    ui.alert("Auditing of the current dataset [[ " + sheetName + " ]] has already begun. Sample cannot be re-generated.");  
+  }
+  else if (contentCount > 20 && twentyPlaceholders.indexOf(sheetName) != -1) {    // If a sheet that should have 20 (auto)filled 
+    var ui = SpreadsheetApp.getUi();                                              // audit cells has more, throw an alert
+    ui.alert("Auditing of the current dataset [[ " + sheetName + " ]] has already begun. Sample cannot be re-generated.");  
+  }
+  else {                                                                    // Otherwise, evaluate whether any matching accounts were found
+    if (sampleData.length > 0) {                                            // If yes, write accounts to the sheet
+      var startRow = 6;
+      var startCol = 1;
+      var lastRow = sampleData.length;
+      var lastCol = sampleData[0].length;
+      var range = sheet.getRange(startRow, startCol, lastRow, lastCol).setValues(sampleData); 
+    }
+    else {                                                                  // If no, fill in a value confirming that were none were found
+      sheet.getRange("A6").setValue("No Matching Accounts Were Found");     // (So there is no confusion that the code actually ran successfully)
+    }    
+  }
+}
+
+
+function writeAllToSheet(categoryData, sheetName) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  if (ss.getSheetByName(sheetName)) {
+    SpreadsheetApp.getUi().alert("That data has already been outputted.");
   }
   else {
-    sheet.getRange("A6").setValue("No Matching Accounts Were Found");
+    ss.insertSheet(sheetName);
+    var sheet = ss.getSheetByName(sheetName);
+    var range = sheet.getRange(1, 1, categoryData.length, 
+                               categoryData[0].length).setValues(categoryData); 
   }
 }
 
-/* CALL ALL FUNCTIONS USING NEW MENU OPTION */
+/* CALL FUNCTIONS TO OUTPUT ALL DATA IN A SINGLE FILTER CATEGORY */
 
-function onboardingTransactionSample() {    // Run all sampler functions
+function writeNoPslCrbAll() {
+  var allValues = getAllValues();
+  var noPslCrbAll = getNoPslCrb(allValues);
+  writeAllToSheet(noPslCrbAll, "[ALL] Funded - NO PSL - CRB");
+}
+
+function writeNoPslBmAll() {
+  var allValues = getAllValues();
+  var noPslBmAll = getNoPslBm(allValues);
+  writeAllToSheet(noPslBmAll, "[ALL] Funded - NO PSL - BankMobile");
+}
+
+function writeYesPslCrbAll() {
+  var allValues = getAllValues();
+  var yesPslCrbAll = getYesPslCrb(allValues);
+  writeAllToSheet(yesPslCrbAll, "[ALL] Funded - PSL - CRB");
+}
+
+function writeDeclinedCrbAll() {
+  var allValues = getAllValues();
+  var declinedCrbAll = getDeclinedCrb(allValues);
+  writeAllToSheet(declinedCrbAll, "[ALL] Declined - CRB");
+}
+
+function writeDeclinedBmAll() {
+  var allValues = getAllValues();
+  var declinedBmAll = getDeclinedBm(allValues);
+  writeAllToSheet(declinedBmAll, "[ALL] Declined - BankMobile");
+}
+
+
+/* CALL FUNCTIONS TO RUN GENERATION OF A SINGLE SAMPLE DATA SET, OR RE-DO IF "__NAME__" == "__MAIN__" */
+
+function runNoPslCrb() {
   var allValues = getAllValues();
   var noPslCrb = getNoPslCrb(allValues);
-  var noPslBm = getNoPslBm(allValues);
-  var yesPslCrb = getYesPslCrb(allValues);
-  var declinedCrb = getDeclinedCrb(allValues);
-  var declinedBm = getDeclinedBm(allValues);
-  
   var noPslCrbSample = randomCrbSample(noPslCrb);
-  var noPslBmSample = randomBmSample(noPslBm);
-  var yesPslCrbSample = randomCrbSample(yesPslCrb);
-  var declinedCrbSample = randomCrbSample(declinedCrb);
-  var declinedBmSample = randomBmSample(declinedBm);
-  
   writeSampleToSheet(noPslCrbSample, "Funded - NO PSL - CRB");
-  writeSampleToSheet(noPslBmSample, "Funded - NO PSL - BM");
-  writeSampleToSheet(yesPslCrbSample, "Funded - PSL - CRB");
-  writeSampleToSheet(declinedCrbSample, "Declined - CRB");
-  writeSampleToSheet(declinedBmSample, "Declined - BM");
 }
+
+function runNoPslBm() {
+  var allValues = getAllValues();
+  var noPslBm = getNoPslBm(allValues);
+  var noPslBmSample = randomBmSample(noPslBm);
+  writeSampleToSheet(noPslBmSample, "Funded - NO PSL - BankMobile");
+}
+
+function runYesPslCrb() {
+  var allValues = getAllValues();
+  var yesPslCrb = getYesPslCrb(allValues);
+  var yesPslCrbSample = randomCrbSample(yesPslCrb);
+  writeSampleToSheet(yesPslCrbSample, "Funded - PSL - CRB");
+}
+
+function runDeclinedCrb() {
+  var allValues = getAllValues();
+  var declinedCrb = getDeclinedCrb(allValues);
+  var declinedCrbSample = randomCrbSample(declinedCrb);
+  writeSampleToSheet(declinedCrbSample, "Declined - CRB");
+}
+
+function runDeclinedBm() {
+  var allValues = getAllValues();
+  var declinedBm = getDeclinedBm(allValues);
+  var declinedBmSample = randomBmSample(declinedBm);
+  writeSampleToSheet(declinedBmSample, "Declined - BankMobile");
+}
+
+
+/* CALL FUNCTIONS TO GENERATE ALL SAMPLES */
+
+function onboardingTransactionSample() {  // Run all sampler functions
+  runNoPslCrb()
+  runNoPslBm()
+  runYesPslCrb()
+  runDeclinedCrb()
+  runDeclinedBm()
+}
+
